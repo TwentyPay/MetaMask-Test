670c6db04c8d249c5b799688a06801a120f5da47
John Sanford <jsanford9292@gmail.com>
2020-10-13 01:32:20 -0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const ethereum = window.ethereum
+  const [address, setAddress] = useState('Not set')
+
+  window.addEventListener('load', async () => {
+    if (ethereum) {
+      try {
+        // await ethereum.enable()
+        ethereum.on('accountsChanged', function (accounts) {
+          console.log("Account was changed!");
+          console.log(accounts[0]);
+          setAddress(accounts[0]);
+        });
+      } catch {
+        console.log("User denied account access")
+      }
+    }
+  });
+
+  async function getAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    setAddress(accounts[0]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="enableEthereumButton"
+        onClick={getAccount}
+      >Enable Ethereum</button>
+      <h2>Account: {address}</h2>
     </div>
   );
 }
